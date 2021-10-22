d4014f389a96b150fba004443134827c9fa0a86d
Tamara Lavandeira <tamaralavandeira@gmail.com>
2021-10-22 00:11:25 -0300

    Landing

--- tickets/src/App.js
+++ tickets/src/App.js
@@ -4,9 +4,11 @@ import NavBar from './components/nav-bar/NavBar';
 import FunctionalComponent from './components/functional-component/FunctionalComponent';
 import { Item } from './components/items/Items';
 import Container from './layouts/Container';
+import ItemListContainer from './layouts/item-list-container/ItemListContainer';
 
 
 const hello = "Hello World"
+const themes = ["red", "blue", "green", "yellow"];
 
 function App() {
 
@@ -15,28 +17,27 @@ function App() {
   const showMessage = () => {
     alert (`Welcome back ${hello} `);
   };
- 
-  const themes = ["red", "blue", "green", "yellow"];
-  
+
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer>
       <h1>Hola {name}</h1>
 
       <ul>
         <Container>
         {themes.map((theme) => (
-          <Item key={theme}> {theme} </Item>
-        )
-        )}
+                <Item key={theme} color={theme} item={theme} />
+              ))}
         </Container>
       </ul>
       <FunctionalComponent 
         hello = {hello}
         setName = {setName}
         showMessage= {showMessage} />
-     <h1>Hola</h1>
+      </ItemListContainer>
     </div>
+   
   );
 }
 
